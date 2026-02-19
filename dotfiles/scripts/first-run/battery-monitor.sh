654130e22d3c9120ba4a682dfbb2a359b2dbd824
aanwidiant <aanwidianto01@gmail.com>
2026-02-19 23:31:20 +0700

    remove first run has been handled by configuration

--- dotfiles/scripts/first-run/battery-monitor.sh
+++
@@ -1,7 +0,0 @@
-if ls /sys/class/power_supply/BAT* &>/dev/null; then
-  powerprofilesctl set balanced || true
-
-  systemctl --user enable --now my-battery-monitor.timer
-else
-  powerprofilesctl set performance || true
-fi
